26340adea8598ec2bb37a9c093d45a6d2387c697
johnAtSales <johnblmk@gmail.com>
2025-01-11 11:55:05 -0500

    Update main.ts

--- smart-server/src/main.ts
+++ smart-server/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow specific origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow cookies or other credentials
+  });
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
